d3ed170fd9d898a223d0d5a80573f496470d1a03
pdegen <peter.methys@gmail.com>
2024-08-20 23:32:02 +0200

    add clusterprofiler test

--- workflow/scripts/run_clusterprofiler.R
+++ workflow/scripts/run_clusterprofiler.R
@@ -1,7 +1,28 @@
-args <- commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 input_file <- args[1]
 output_file <- args[2]
 
 library(clusterProfiler)
+library(org.Hs.eg.db)
 
-write.csv(as.data.frame(c(0,0,0,0)), file=output_file, row.names=FALSE)
+
+metrics <- c("neg_signed_logpval", "logFC")
+
+print(input_file)
+df <- read.csv(input_file, row.names = 1)
+
+for (metric in metrics) {
+    # Check if the metric is in the columns
+    if (!(metric %in% colnames(df))) {
+        if (metric == "neg_signed_logpval") {
+            message(paste("Adding", metric, "to df"))
+            df$neg_signed_logpval <- -sign(df$logFC) * log10(df$PValue)
+        } else {
+            stop(paste("Metric", metric, "not in columns!"))
+        }
+    }
+}
+
+print(head(df))
+
+write.csv(as.data.frame(c(0, 0, 0, 0)), file = output_file, row.names = FALSE)
