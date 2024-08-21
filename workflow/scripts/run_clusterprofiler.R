3c6b6ce244c21994d1a0a6a70f9b8cee6f2165d6
pdegen <peter.methys@gmail.com>
2024-08-21 17:39:14 +0200

    add mus musculus

--- workflow/scripts/run_clusterprofiler.R
+++ workflow/scripts/run_clusterprofiler.R
@@ -1,11 +1,12 @@
 library(clusterProfiler)
-library(org.Hs.eg.db)
 
 args <- commandArgs(trailingOnly = TRUE)
 input_file <- args[1]
 outfile_go <- args[2]
 outfile_kegg <- args[3]
 metric <- args[4]
+keytype <- args[5]
+organismKEGG <- args[6]
 
 print(paste("Reading clsuterProfiler input:", input_file))
 
@@ -22,9 +23,10 @@ if (!(metric %in% colnames(df))) {
 }
 
 run_clusterProfiler <- function(df, outfile_go, outfile_kegg,
-                                metric, overwrite=FALSE, 
-                                organism.KEGG="hsa",
-                                organism.GO = org.Hs.eg.db, seed=123) 
+                                metric,
+                                organism.KEGG,
+                                organism.GO, 
+                                overwrite=FALSE, seed=1234) 
 {
   set.seed(seed)
 
@@ -77,17 +79,28 @@ convert_df <- function(df, OrgDb=org.Hs.eg.db) {
 
   if ("ENTREZID" %in% names(df)) return(df)
   
-  df$ENSEMBL <- row.names(df)
+  df[[keytype]] <- row.names(df)
   # Convert to ENTREZ ID
   # We will lose some genes here because not all IDs will be converted
 
-  ids<-bitr(row.names(df), fromType = "ENSEMBL", toType = "ENTREZID", OrgDb=OrgDb)
-  df <- merge(df, ids, by = "ENSEMBL", all.x = TRUE)
+  ids<-bitr(row.names(df), fromType = keytype, toType = "ENTREZID", OrgDb=OrgDb)
+  df <- merge(df, ids, by = keytype, all.x = TRUE)
   print(paste("Before",nrow(df)))
   df <- na.omit(df)
   print(paste("After",nrow(df)))
   return(df)
 }
 
-df <- convert_df(df, OrgDb=org.Hs.eg.db)
-run_clusterProfiler(df, outfile_go, outfile_kegg, metric, overwrite=FALSE, organism.KEGG="hsa", organism.GO = org.Hs.eg.db) 
+if (organismKEGG == "hsa") {
+    library(org.Hs.eg.db)
+    OrgDb <- org.Hs.eg.db
+} else if (organismKEGG == "mmu") {
+    library(org.Mm.eg.db)
+    OrgDb <- org.Mm.eg.db
+} else {
+    stop(paste("Organism not yet implemented:", organismKEGG))
+}
+
+df <- convert_df(df, OrgDb=OrgDb)
+print(head(df))
+run_clusterProfiler(df, outfile_go, outfile_kegg, metric, overwrite=FALSE, organism.KEGG=organismKEGG, organism.GO = OrgDb) 
