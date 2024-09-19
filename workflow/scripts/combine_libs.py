6826982dd3122a0087762e52173e492dc2923bd3
pdegen <peter.methys@gmail.com>
2024-09-19 19:51:11 +0200

    refactor save depth df

--- workflow/scripts/combine_libs.py
+++ workflow/scripts/combine_libs.py
@@ -1,4 +1,4 @@
-# scripts/combine_results.py
+# scripts/combine_libs.py
 
 import os
 import yaml
@@ -22,7 +22,9 @@ def create_summary_dict(savepath: str,
         summary_df.sort_values(by=("Combined","nan","Combined FDR"))
         summary_dict[lib]["summary_df"] = summary_df
         sig_dict = get_sig_dict(summary_df, tools, metrics, qval=qval, verbose=True)
-        summary_dict[lib]["depth_df"] = create_intersection_depth_df(sig_dict)    
+        depth_df = create_intersection_depth_df(sig_dict)
+        depth_df = format_depth_df(depth_df, project_name, savepath, lib, summary_df)
+        summary_dict[lib]["depth_df"] = depth_df
 
     # Store results in dictionary for meta-analysis
     if save:
@@ -31,6 +33,44 @@ def create_summary_dict(savepath: str,
         pickler(summary_dict, outfile)
 
     return summary_dict
+
+def format_depth_df(depth_df: pd.DataFrame,
+                    project_name: str,
+                    savepath: str,
+                    lib: str,
+                    summary_df: pd.DataFrame
+                    ) -> pd.DataFrame:
+    
+    outfile = os.path.join(savepath, f"syn.depth.{lib}.{project_name}.csv")
+    d = depth_df
+    if len(d) < 1:
+        print(f"No terms found for {lib}")
+        pd.DataFrame(f"No terms found for {lib}").to_csv(outfile)
+        return d
+    
+    # Append GO sub-ontology
+    if d.index[0].startswith("GO:"):
+        d["ID"] = d.index.str.split("_").str[0]
+        ont = pd.DataFrame(summary_df[("nan","nan","ONTOLOGY")].values, index=summary_df.index, columns=["ONTOLOGY"])
+        d = d.merge(ont["ONTOLOGY"], left_on="ID", right_index=True, how="left")
+
+    if "Direction" not in d:
+        d["Direction"] = d.index.str.split("_").str[1].str.strip()
+
+    d.index = d.index.str.split("_").str[0]
+    d.rename({"Factors":"Configurations"}, axis=1, inplace=True)
+    d["Combined FDR"] = summary_df.loc[d.index,("Combined","nan","Combined FDR")]
+
+    d.sort_values(by=["Depth","Combined FDR"], ascending=[False,True], inplace=True)
+
+    cols = ["Description","Depth","Direction","Combined FDR"]
+    if "ONTOLOGY" in d.columns:
+        cols.append("ONTOLOGY")
+    cols.append("Configurations")
+
+    d = d[cols]
+    d.to_csv(outfile)
+    return d
 
 if __name__ == "__main__":
 
@@ -48,6 +88,7 @@ if __name__ == "__main__":
     savepath = os.path.join("results",project_name,"combined")
 
     create_summary_dict(savepath, libs, tools, metrics, project_name, qval, save)
+
 
 
 
